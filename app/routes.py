0756a28ba82fef12c03e20f1b451973eba76a4c7
colonelrascals <pdhildreth@gmail.com>
2018-01-17 15:52:20 -0600

    add flask-wtf

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,30 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
-
 def index():
-    user = { 'username': 'Patrick'}
+    user = {'username': 'Miguel'}
     posts = [
         {
-            'author': {'username': 'Kenji'},
-            'body': 'Beautiful day in Houston!'
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
         },
         {
-            'author': {'username': 'Josh'},
-            'body': 'I love D&D!'
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',  title='Sign In', form=form)
